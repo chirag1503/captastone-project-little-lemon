5e9585339c3a14dfd6d3bb56d28136405dbd3109
chirag1503 <cdagrawal551@gmail.com>
2024-05-08 11:02:54 +0530

    Resolved merge conflicts

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // or use HashRouter if preferred
+import './App.css';
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
